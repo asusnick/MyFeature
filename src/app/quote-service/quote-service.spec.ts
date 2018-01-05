589b18c521d2cb571787b21d6de99ce9b98afd4e
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2018-01-05 11:15:29 -0500

    updated service test

--- src/app/quote-service/quote-service.spec.ts
+++ src/app/quote-service/quote-service.spec.ts
@@ -1,19 +1,34 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject, getTestBed} from '@angular/core/testing';
 import { QuoteService} from './quote-service';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpTestingController} from '@angular/common/http/testing';
+import {afterEach} from 'selenium-webdriver/testing';
 
 describe('QuoteService', () => {
+  let injector: TestBed;
+  let service: QuoteService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule ],
       providers: [QuoteService]
     });
+    injector = getTestBed();
+    service = injector.get(QuoteService);
+    httpMock = injector.get(HttpTestingController);
   });
 
   it('should be created', inject([QuoteService], (service: QuoteService) => {
     expect(service).toBeTruthy();
   }));
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 });
+
+
 
 
